c855b848e4bffff0011366f016162266047e31f7
Quincy van Stenus <quincy@Quincys-MacBook-Pro.local>
2025-04-14 16:47:05 +0200

    fixed db issues

--- srcs/requirements/mariadb/tools/main.sh
+++ srcs/requirements/mariadb/tools/main.sh
@@ -1,31 +1,26 @@
 #!/bin/ash
 
-set -e
-
 if [ ! -f "$DIR_DATA/ibdata1" ]; then
-    mariadb-install-db \
-        --datadir=$DIR_DATA \
-        --auth-root-authentication-method=socket &
+    mariadb-install-db --datadir=${DIR_DATA} &
     wait $!
     
-    mariadbd --datadir=$DIR_DATA  &
-    PID=$!
+    mariadbd --datadir=${DIR_DATA} --skip-networking  &
     
-    until mariadb-admin ping 
-    do
+    until mariadb-admin ping 2>/dev/null; do
         sleep 1
     done
-    
+
     mariadb <<-EOF
         CREATE DATABASE IF NOT EXISTS ${DB_NAME};
         CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
         GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
         DELETE FROM mysql.user WHERE User='';
+        DROP DATABASE IF EXISTS test;
         FLUSH PRIVILEGES; 
 EOF
 
-    kill $PID
-    wait $PID || true
+    mariadb-admin  shutdown
+    wait $!
 fi
 
-exec /usr/bin/mariadbd-safe --datadir=$DIR_DATA
+exec /usr/bin/mariadbd-safe --datadir=${DIR_DATA}
